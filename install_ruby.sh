2b8a36c4bb8c649350d422550fce056b8add98ae
errormess <91407868+errormess@users.noreply.github.com>
2022-11-21 02:03:25 +0300

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
- sudo apt update
- sudo apt install -y ruby-full ruby-bundler build-essential
- ruby -v
- ruby_v=$?
- bundler -v
- bundler_v=$?
- if [[ "$ruby_v" -eq 0 ]] && [[ "$bundler_v" -eq 0 ]]; then
-   echo "ruby installed"
- else
-   echo "FAILED ruby install"
- fi
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y ruby-full ruby-bundler build-essential
