39e13a6f02179716f114f102a514491784698835
Mr.Otus <internal@internet.ru>
2022-11-21 02:11:20 +0300

    ADD mongodb,ruby,deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
- sudo apt install -y git
- git clone -b monolith https://github.com/express42/reddit.git
- cd reddit && bundle install
- puma -d
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
